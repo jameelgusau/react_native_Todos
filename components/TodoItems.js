4e3d06fcae27858fd9c0760b7e51f744a21ba98f
shehu Jamilu <stetis@jameel.local>
2021-05-07 12:00:39 +0100

    icons added

--- components/TodoItems.js
+++ components/TodoItems.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function TodoItems({item, handlePress}) {
-    return (
-        <TouchableOpacity onPress={()=>handlePress(item.id)}>
-            <Text style={styles.item }>{item.label}</Text>
-        </TouchableOpacity>
-    )
+export default function TodoItems({ item, handlePress }) {
+  return (
+    <TouchableOpacity onPress={() => handlePress(item.id)}>
+      <View  style={styles.item}>
+        <MaterialIcons name="delete" size={18} color="#333"/>
+        <Text  style={styles.itemText}>{item.label}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    item:{
-        padding: 16,
-        marginTop: 16,
-        borderStyle: 'dashed',
-        borderColor: "#bbb",
-        borderWidth: 1,
-        borderRadius: 10,
-        
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderStyle: "dashed",
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderRadius: 10,
+    flexDirection: "row",
 
-    }
-})
+  },
+  itemText:{
+      marginLeft: 10,
+      
+  }
+});
